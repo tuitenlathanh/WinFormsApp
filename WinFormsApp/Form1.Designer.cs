dc92d0b9cd28458250e85f2c0d91d1cb91e37f20
Thành Đinh Trọng <admin@thanhdeptrai1.onmicrosoft.com>
2023-06-09 09:50:46 +0700

    dxassdsadsd

---
+++ WinFormsApp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DocFile = new Button();
+            GhiFile = new Button();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // DocFile
+            // 
+            DocFile.Location = new Point(160, 389);
+            DocFile.Name = "DocFile";
+            DocFile.Size = new Size(94, 29);
+            DocFile.TabIndex = 0;
+            DocFile.Text = "Upload";
+            DocFile.UseVisualStyleBackColor = true;
+            DocFile.Click += DocFile_Click;
+            // 
+            // GhiFile
+            // 
+            GhiFile.Location = new Point(408, 389);
+            GhiFile.Name = "GhiFile";
+            GhiFile.Size = new Size(94, 29);
+            GhiFile.TabIndex = 1;
+            GhiFile.Text = "DocFile.txt";
+            GhiFile.UseVisualStyleBackColor = true;
+            GhiFile.Click += GhiFile_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(204, 50);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 2;
+            label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(GhiFile);
+            Controls.Add(DocFile);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button DocFile;
+        private Button GhiFile;
+        private Label label1;
+    }
+}
---
+++ WinFormsApp/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // private static Label? Label1; // Label để hiển thị trạng thái
+        private void DocFile_Click(object sender, EventArgs e)
+        {
+            // Tạo một OpenFileDialog để cho phép người dùng chọn thư mục
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Chọn thư mục";
+                openFileDialog.CheckFileExists = false;
+                openFileDialog.CheckPathExists = true;
+                openFileDialog.FileName = "Thư mục";
+                openFileDialog.ValidateNames = false;
+                openFileDialog.FileName = "Thư mục";
+                openFileDialog.Filter = "Thư mục|*.";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string folderPath = Path.GetDirectoryName(openFileDialog.FileName);
+
+                    // Hiển thị trạng thái và bắt đầu quá trình tính toán trong luồng mới
+                    label1.Visible = true;
+                    Task.Run(() => CalculateHashesAndSaveToFile(folderPath));
+                }
+            }
+        }
+
+        private void CalculateHashesAndSaveToFile(string folderPath)
+        {
+            try
+            {
+                // Tạo tên file .txt và đường dẫn đến file
+                string txtFilePath = Path.Combine(folderPath, "file_list.txt");
+
+                // Lấy danh sách các file trong thư mục
+                string[] files = Directory.GetFiles(folderPath);
+
+                // Tạo một StringBuilder để xây dựng nội dung file .txt
+                StringBuilder sb = new StringBuilder();
+
+                // Tạo MD5 hash cho từng file và ghi vào StringBuilder
+                foreach (string file in files)
+                {
+                    string fileName = Path.GetFileName(file);
+                    string hash = CalculateMD5(file);
+                    sb.AppendLine($"{fileName} - {hash}");
+                }
+
+                // Ghi nội dung StringBuilder vào file .txt
+                File.WriteAllText(txtFilePath, sb.ToString());
+
+                // Hiển thị thông báo hoàn thành trên giao diện người dùng
+                label1.BeginInvoke((Action)(() =>
+                {
+                    label1.Text = "Hoàn thành!";
+                }));
+            }
+            catch (Exception ex)
+            {
+                // Xử lý lỗi nếu có
+                MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+            }
+        }
+
+
+
+        private static string CalculateMD5(string filePath)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    byte[] hashBytes = md5.ComputeHash(stream);
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < hashBytes.Length; i++)
+                    {
+                        sb.Append(hashBytes[i].ToString("x2"));
+                    }
+                    return sb.ToString();
+                }
+            }
+        }
+
+        private void GhiFile_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Title = "Chọn file văn bản";
+                openFileDialog.Filter = "Text Files|*.txt";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+
+                    try
+                    {
+                        string content = File.ReadAllText(filePath);
+                        //textBoxContent.Text = content;
+                        label1.Text = content;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Đã xảy ra lỗi: " + ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
